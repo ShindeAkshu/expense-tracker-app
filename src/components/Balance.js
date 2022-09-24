dc52e6807906a853880c9369fe6376dfd94284be
shindeAkshu <sakshata416@gmail.com>
2022-09-24 11:05:07 -0700

    Expense tracker

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,9 +5,7 @@ import Transaction from './Transaction';
 
 const Balance = () => {
 
-  const amounts = Transaction.map(transaction => transaction.amount);
-  const total = amounts.reduce((acc,item) => (acc += item),0).toFixed(2);
-
+ 
   return (
     <>
       <h4>Your Balance</h4>
