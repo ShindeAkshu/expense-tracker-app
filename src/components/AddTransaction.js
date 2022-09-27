9fe39f326642e8422a6ee3e83eb7f1b878b14c6d
shindeAkshu <sakshata416@gmail.com>
2022-09-27 11:11:21 -0700

    Expense Tracker

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -7,7 +7,10 @@ const AddTransaction = () => {
     <>
       <h3>Add new transaction</h3>
       <form >
-        
+        <div className="form-control">
+            <label htmlFor="text">Text</label>
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter text...'/>
+        </div>
         <div className="form-control">
             <label htmlFor="amount">Amount<br/>(negative - expense,positive - income)</label>
             <input type="number"  value={amount} onChange={(e) => setAmount(e.target.value)}
